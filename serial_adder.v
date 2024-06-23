e8b81b75989c850f64f2125c4f41cdc33399c57e
va-rox <97227132+va-rox@users.noreply.github.com>
2024-06-23 15:25:03 +0530

    4 bit serial adder updated

--- serial_adder.v
+++ serial_adder.v
@@ -22,3 +22,43 @@ module serial_adder (
   assign carry_out = carry_reg;
 
 endmodule
+
+module serial_adder_4bit (sum, clk, rst, a, b);
+input clk, rst;
+input [3:0] a, b;
+output reg [4:0] sum;
+reg [3:0] ina, inb;
+reg [4:0] isum;
+reg s, c;
+
+  
+
+  // Input registers for data synchronization
+always @(posedge clk) begin
+	
+	if (rst) begin
+		ina = a;
+		inb = b;
+		isum = 0;
+	end
+	
+	else begin
+	
+		ina = {1'b0, ina [3:1]};
+		inb = {1'b0, inb [3:1]};
+		isum[4] = c;
+		isum[3:0] = {s, isum [3:1]};
+	
+	end
+	
+	
+	s = ina[0] ^ inb[0] ^ isum[4];
+	c = (ina[0]&inb[0])|(ina[0]&isum[4])|(inb[0]&isum[4]);
+	 sum = isum;
+    
+	 
+end
+
+endmodule
+
+
